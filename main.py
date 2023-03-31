5c69305e8e3f21b141c6930fdcfa5ad1fd778e49
Suraj Wijewickrama <90107939+SurajWijewickrama@users.noreply.github.com>
2023-03-31 12:59:56 +0530

    initial commit added api logic

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from auth_token import auth_token
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from io import BytesIO
+import base64 
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+app.add_middleware(
+    CORSMiddleware, 
+    allow_credentials=True, 
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
 
+device = "cuda"
+model_id = "CompVis/stable-diffusion-v1-4"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token)
+pipe.to(device)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+def generate(prompt: str): 
+    with autocast(device): 
+        image = pipe(prompt, guidance_scale=8.5).images[0]
 
+    image.save("testimage.png")
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")
+    imgstr = base64.b64encode(buffer.getvalue())
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+    return Response(content=imgstr, media_type="image/png")
